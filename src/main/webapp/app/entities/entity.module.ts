f22613693d5f96baaca7d9be7ed0cf1f31dac3c8
ffacon <francois.facon@gmail.com>
2017-10-27 16:51:50 +0200

    Update LiquidBase and cacheconfiguration

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,11 +1,17 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { HealthPointsModule } from './points/points.module';
+import { HealthWeightModule } from './weight/weight.module';
+import { HealthBloodpressureModule } from './bloodpressure/bloodpressure.module';
+import { HealthPreferencesModule } from './preferences/preferences.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
         HealthPointsModule,
+        HealthWeightModule,
+        HealthBloodpressureModule,
+        HealthPreferencesModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
